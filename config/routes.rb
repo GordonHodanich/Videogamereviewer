848bca39839fd970be5223874ad350f874edd734
Gordon Hodanich <ghodanich@gmail.com>
2021-10-22 14:43:33 -0400

    added my comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:new, :create]
+  resources :comments, only: [:new, :create, :show, :index]
   resources :games, only: [:index, :show, :new, :create] do 
     resources :comments, only: [:create]
   end
+
   devise_for :users
   root to: "home#index"
 end
