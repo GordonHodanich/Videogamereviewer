179d553d875b65495f44aab2fd02294e7656de31
Gordon Hodanich <ghodanich@gmail.com>
2021-10-22 11:30:14 -0400

    done but not complete

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments
+  validates :rank, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
